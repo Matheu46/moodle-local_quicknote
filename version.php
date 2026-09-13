501755745d0dd77afec6500c6642819f4a837ece
Matheus Mathias <mathias.matheus@ymail.com>
2026-09-12 21:54:36 -0300

    chore: update plugin release version to m45-0.11.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_quicknote';
-$plugin->release = '0.10.0';
+$plugin->release = 'm45-0.11.0';
 $plugin->version = 2026091200;
 $plugin->requires = 2023042400; // Moodle 4.2.
 $plugin->maturity = MATURITY_BETA;
